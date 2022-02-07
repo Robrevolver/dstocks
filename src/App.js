56b0220689961a003968f0a867ba749a4854eca6
Robrevolver <rstoeppel@stoeppelgroup.de>
2022-02-07 20:09:25 +0100

    semantic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , { useEffect, useState} from 'react';
+import React , { useEffect, useState } from 'react';
 import ocean from './components/OceanClient'
 import { getOraclePrice, getDexPrice, getPremium } from './commons/functions';
 import { dStocks } from './commons/dstocks'
@@ -13,10 +13,10 @@ const App = () => {
             const oraclePrices = await ocean.prices.list(90)
             const oraclePriceList = oraclePrices.map(item => [item.price.token, item.price.aggregated.amount])
             setOraclePriceList(oraclePriceList)
-            // console.log(oraclePriceList)
+            // console.log(oraclePrices)
 
             const dexPrices = await ocean.poolpairs.list(90)
-            const dexPriceList = dexPrices.map(item => [item.tokenA.symbol, item.priceRatio.ba])
+            const dexPriceList = dexPrices.map(item => [item.tokenA.symbol, item.priceRatio.ba, item.totalLiquidity.usd])
             setDexPriceList(dexPriceList)
             // console.log(dexPriceList)               
             }
@@ -38,18 +38,20 @@ const App = () => {
 
   return (
     
-    <div>
+    <div className = "ui container">
       <h1>dStocks</h1>
       <div></div>
-      <div>{dStocksList(oraclePriceList, dexPriceList, dStocks).map(dStock => 
-        <div key={dStock.name}>
-          {`${dStock.name} - ${dStock.oraclePrice} - ${dStock.dexPrice} - ${dStock.ratio}`}
-        </div>)
+      <div>{dStocksList(oraclePriceList, dexPriceList, dStocks)
+                .filter(item => item.ratio > 25).sort((a,b) => b.ratio - a.ratio)
+                .map(dStock => 
+                <div key={dStock.name}>
+                  {`${dStock.symbol} - ${dStock.name} - ${dStock.oraclePrice} - ${dStock.dexPrice} - ${dStock.ratio}`}
+                </div>
+                )
         }
       </div>
     </div>
   );
 }
-
 
 export default App;
